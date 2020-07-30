ef5ca1702fa0284347afa9cb36f27c6a998e95f4
LAPTOP-SG4LQ13C\赵晓凡 <fanShaogithub@126.com>
2020-07-30 17:23:52 +0800

    整体布局css
    Signed-off-by: LAPTOP-SG4LQ13C\赵晓凡 <fanShaogithub@126.com>

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,17 +1,19 @@
 <template>
-  <div>
+  <div class="g-doc">
     <topNav></topNav>
-    <div class="content">
-      <aside v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li><router-link to="/doc/switch">Switch 组件</router-link></li>
-          <li><router-link to="/doc/button">Button 组件</router-link></li>
-          <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
-          <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
-        </ol>
-      </aside>
-      <main><router-view /></main>
+    <div class="content-box">
+      <div class="content">
+        <aside v-if="menuVisible">
+          <h2>组件列表</h2>
+          <ol>
+            <li><router-link to="/doc/switch">Switch 组件</router-link></li>
+            <li><router-link to="/doc/button">Button 组件</router-link></li>
+            <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
+            <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
+          </ol>
+        </aside>
+        <main><router-view /></main>
+      </div>
     </div>
   </div>
 </template>
@@ -30,12 +32,32 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.g-doc {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.content-box {
+  position: absolute;
+  top: 80px;
+  bottom: 0;
+  width: 100%;
+  background: chocolate;
+}
  .content {
+   display: flex;
+   justify-content: space-between;
+   background: lightcyan;
+   width: 100%;
+   height: 100%;
    aside {
      width: 150px;
+     height: 100%;
      background: lightblue;
      color: black;
      padding: 16px;
+     margin-right: -150px;
+     box-sizing: border-box;
      ol {
        li { 
          text-align: left;
@@ -46,7 +68,17 @@ export default {
       position: fixed;
       top: 0;
       left: 0;
-      padding-top: 70px;
+      padding-top: 86px;
+    }
+   }
+   main {
+     margin-left: 150px;
+     float: right;
+     width: 100%;
+     height: 100%;
+     overflow: scroll;
+     @media (max-width: 500px) {
+      margin-left: 0;
     }
    }
  } 
